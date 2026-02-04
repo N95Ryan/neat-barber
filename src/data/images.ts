a25ef51532e6612f939438a1c695385b717ac657
Ryan Pina-silasse <ryanpinas77@gmail.com>
2026-02-04 11:41:05 +0100

    feat/ui : 'Refactor PhotoGallery avec carrousel infini fluide et optimisation Cloudinary'

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,32 +1,51 @@
 // src/data/images.ts
 export const galleryImages = [
     {
-      url: "https://res.cloudinary.com/drhqmgbm0/image/upload/f_auto,q_auto,w_800/v1755728029/1_oajndh.jpg",
-      alt: "Salon Neat Barber - Vue d'ensemble"
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1755728029/1_oajndh.jpg",
+        alt: "Salon Neat Barber - Vue d'ensemble"
     },
     {
-      url: "https://res.cloudinary.com/drhqmgbm0/image/upload/f_auto,q_auto,w_800/v1756349811/2_dq83e4.webp",
-      alt: "Salon Neat Barber - Espace de travail"
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1768996216/11_g28tzh.jpg",
+        alt: "Salon Neat Barber - Devanture du salon"
     },
     {
-      url: "https://res.cloudinary.com/drhqmgbm0/image/upload/f_auto,q_auto,w_800/v1755728033/3_mbs6k1.jpg",
-      alt: "Salon Neat Barber - Ambiance salon"
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1768996216/15_ru4hgz.jpg",
+        alt: "Salon Neat Barber - Caisse"
     },
     {
-      url: "https://res.cloudinary.com/drhqmgbm0/image/upload/f_auto,q_auto,w_800/v1756349812/4_q3efgd.webp",
-      alt: "Salon Neat Barber - Zone d'accueil"
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1768996213/10_dhic1g.jpg",
+        alt: "Salon Neat Barber - Fenêtre"
     },
     {
-      url: "https://res.cloudinary.com/drhqmgbm0/image/upload/f_auto,q_auto,w_800/v1756349812/5_qxx40g.webp",
-      alt: "Salon Neat Barber - Détails d'équipement"
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1768996216/12_p12d7n.jpg",
+        alt: "Salon Neat Barber - Étagère murale"
     },
     {
-      url: "https://res.cloudinary.com/drhqmgbm0/image/upload/f_auto,q_auto,w_800/v1756349810/6_sa5zri.webp",
-      alt: "Salon Neat Barber - Espace détente"
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1768996214/5_iivyo3.jpg",
+        alt: "Salon Neat Barber - Produits Bullfrog"
     },
     {
-      url: "https://res.cloudinary.com/drhqmgbm0/image/upload/f_auto,q_auto,w_800/v1756349809/7_uom9of.webp",
-      alt: "Salon Neat Barber - Vue complète"
-    }
-  ];
-  
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1768996221/13_to7trp.jpg",
+        alt: "Salon Neat Barber - Équipement"
+    },
+    {
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1768996214/5_iivyo3.jpg",
+        alt: "Salon Neat Barber - Fauteuils"
+    },
+    {
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1768996229/9_dr3ehx.jpg",
+        alt: "Salon Neat Barber - Espace de travail"
+    },
+    {
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1755728033/3_mbs6k1.jpg",
+        alt: "Salon Neat Barber - Ambiance salon"
+    },
+    {
+        url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1756349810/6_sa5zri.webp",
+        alt: "Salon Neat Barber - Espace détente"
+    },
+    // {
+    //     url: "https://res.cloudinary.com/drhqmgbm0/image/upload/v1756349809/7_uom9of.webp",
+    //     alt: "Salon Neat Barber - Vue complète"
+    // }
+];
